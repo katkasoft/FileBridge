1a03da776a6eda7563da78d76cd75c8884f07940
katkasoft <katkaleks@gmail.com>
2026-01-19 20:34:34 +0300

    Ready

--- gui/get.py
+++ gui/get.py
@@ -2,6 +2,8 @@ import threading
 import subprocess
 import customtkinter as ctk
 from CTkListbox import CTkListbox
+import requests
+from pathlib import Path
 
 class GetTab(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -30,6 +32,21 @@ class GetTab(ctk.CTkFrame):
         for line in self.process.stdout:
             self.listbox.insert("end", line.strip())
 
+    def get(self):
+        threading.Thread(target=self._run_get, daemon=True).start()
+
+    def _run_get(self):
+        ip = self.ip_entry.get()
+        url = "http://"+ip+":1928"
+        res = requests.get(url+"/filename")
+        filename = res.text
+        download_path = Path.home() / "Загрузки" / filename
+        with requests.get(url+"/file", stream=True) as r:
+            with open(download_path, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+
     def setup_ui(self):
         self.ip_scan_frame = ctk.CTkFrame(self)
         self.ip_scan_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
@@ -46,5 +63,5 @@ class GetTab(ctk.CTkFrame):
         self.ip_entry = ctk.CTkEntry(self.ip_get_frame, placeholder_text="Enter ip...")
         self.ip_entry.pack(padx=10, pady=(50, 10), fill="x")
 
-        self.get_btn = ctk.CTkButton(self.ip_get_frame, text="Get file", state="disabled")
+        self.get_btn = ctk.CTkButton(self.ip_get_frame, text="Get file", command=self.get)
         self.get_btn.pack(padx=10, pady=10, fill="x")
